f9c2ceb9fcf16532260867265d475aa1a22611f4
DESKTOP-TC7QSU2\Hrvoje <hsostaric@foi.hr>
2018-07-03 10:25:20 +0200

    Formi Otplatna tablica dodano svojstvo template

--- MobiFinance/Login/Forme/FrmOtplatnaTablica.cs
+++ MobiFinance/Login/Forme/FrmOtplatnaTablica.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Login.Forme
 {
-    public partial class FrmOtplatnaTablica : Form
+    public partial class FrmOtplatnaTablica : Template
     {
         public FrmOtplatnaTablica()
         {
