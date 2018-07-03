65915d459cfb8b3616066814c5febc167984a952
DESKTOP-TC7QSU2\Hrvoje <hsostaric@foi.hr>
2018-07-03 12:11:58 +0200

    Urešivanje forme OtplatnaTablica

--- MobiFinance/Login/Forme/FrmOtplatnaTablica.Designer.cs
+++ MobiFinance/Login/Forme/FrmOtplatnaTablica.Designer.cs
@@ -34,8 +34,19 @@
             this.kreditBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dgvOtplatnaTablica = new System.Windows.Forms.DataGridView();
             this.btnPrintanje = new System.Windows.Forms.Button();
+            this.otplatnatablicaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.brojMjesecaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kamateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.anuitetDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.otplatnaKvotaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ostatakDugaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.otplaceniDioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kreditidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kreditDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.kreditBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOtplatnaTablica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.otplatnatablicaBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // cbPopisKredita
@@ -56,10 +67,27 @@
             // 
             // dgvOtplatnaTablica
             // 
+            this.dgvOtplatnaTablica.AllowUserToAddRows = false;
+            this.dgvOtplatnaTablica.AllowUserToDeleteRows = false;
+            this.dgvOtplatnaTablica.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvOtplatnaTablica.AutoGenerateColumns = false;
             this.dgvOtplatnaTablica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvOtplatnaTablica.Location = new System.Drawing.Point(12, 177);
+            this.dgvOtplatnaTablica.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.brojMjesecaDataGridViewTextBoxColumn,
+            this.idDataGridViewTextBoxColumn,
+            this.kamateDataGridViewTextBoxColumn,
+            this.anuitetDataGridViewTextBoxColumn,
+            this.otplatnaKvotaDataGridViewTextBoxColumn,
+            this.ostatakDugaDataGridViewTextBoxColumn,
+            this.otplaceniDioDataGridViewTextBoxColumn,
+            this.kreditidDataGridViewTextBoxColumn,
+            this.kreditDataGridViewTextBoxColumn});
+            this.dgvOtplatnaTablica.DataSource = this.otplatnatablicaBindingSource;
+            this.dgvOtplatnaTablica.Location = new System.Drawing.Point(30, 180);
             this.dgvOtplatnaTablica.Name = "dgvOtplatnaTablica";
-            this.dgvOtplatnaTablica.Size = new System.Drawing.Size(655, 214);
+            this.dgvOtplatnaTablica.Size = new System.Drawing.Size(643, 214);
             this.dgvOtplatnaTablica.TabIndex = 1;
             // 
             // btnPrintanje
@@ -75,6 +103,67 @@
             this.btnPrintanje.Text = "Printanje";
             this.btnPrintanje.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnPrintanje.UseVisualStyleBackColor = true;
+            // 
+            // otplatnatablicaBindingSource
+            // 
+            this.otplatnatablicaBindingSource.DataSource = typeof(Login.Otplatna_tablica);
+            // 
+            // brojMjesecaDataGridViewTextBoxColumn
+            // 
+            this.brojMjesecaDataGridViewTextBoxColumn.DataPropertyName = "BrojMjeseca";
+            this.brojMjesecaDataGridViewTextBoxColumn.HeaderText = "Broj mjeseca";
+            this.brojMjesecaDataGridViewTextBoxColumn.Name = "brojMjesecaDataGridViewTextBoxColumn";
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // kamateDataGridViewTextBoxColumn
+            // 
+            this.kamateDataGridViewTextBoxColumn.DataPropertyName = "Kamate";
+            this.kamateDataGridViewTextBoxColumn.HeaderText = "Kamate";
+            this.kamateDataGridViewTextBoxColumn.Name = "kamateDataGridViewTextBoxColumn";
+            // 
+            // anuitetDataGridViewTextBoxColumn
+            // 
+            this.anuitetDataGridViewTextBoxColumn.DataPropertyName = "Anuitet";
+            this.anuitetDataGridViewTextBoxColumn.HeaderText = "Anuitet";
+            this.anuitetDataGridViewTextBoxColumn.Name = "anuitetDataGridViewTextBoxColumn";
+            // 
+            // otplatnaKvotaDataGridViewTextBoxColumn
+            // 
+            this.otplatnaKvotaDataGridViewTextBoxColumn.DataPropertyName = "OtplatnaKvota";
+            this.otplatnaKvotaDataGridViewTextBoxColumn.HeaderText = "Otplatna kvota";
+            this.otplatnaKvotaDataGridViewTextBoxColumn.Name = "otplatnaKvotaDataGridViewTextBoxColumn";
+            // 
+            // ostatakDugaDataGridViewTextBoxColumn
+            // 
+            this.ostatakDugaDataGridViewTextBoxColumn.DataPropertyName = "OstatakDuga";
+            this.ostatakDugaDataGridViewTextBoxColumn.HeaderText = "Ostatak duga";
+            this.ostatakDugaDataGridViewTextBoxColumn.Name = "ostatakDugaDataGridViewTextBoxColumn";
+            // 
+            // otplaceniDioDataGridViewTextBoxColumn
+            // 
+            this.otplaceniDioDataGridViewTextBoxColumn.DataPropertyName = "OtplaceniDio";
+            this.otplaceniDioDataGridViewTextBoxColumn.HeaderText = "Otplaceni dio";
+            this.otplaceniDioDataGridViewTextBoxColumn.Name = "otplaceniDioDataGridViewTextBoxColumn";
+            // 
+            // kreditidDataGridViewTextBoxColumn
+            // 
+            this.kreditidDataGridViewTextBoxColumn.DataPropertyName = "Kredit_id";
+            this.kreditidDataGridViewTextBoxColumn.HeaderText = "Kredit_id";
+            this.kreditidDataGridViewTextBoxColumn.Name = "kreditidDataGridViewTextBoxColumn";
+            this.kreditidDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // kreditDataGridViewTextBoxColumn
+            // 
+            this.kreditDataGridViewTextBoxColumn.DataPropertyName = "Kredit_";
+            this.kreditDataGridViewTextBoxColumn.HeaderText = "Kredit_";
+            this.kreditDataGridViewTextBoxColumn.Name = "kreditDataGridViewTextBoxColumn";
+            this.kreditDataGridViewTextBoxColumn.Visible = false;
             // 
             // FrmOtplatnaTablica
             // 
@@ -92,6 +181,7 @@
             this.Controls.SetChildIndex(this.btnPrintanje, 0);
             ((System.ComponentModel.ISupportInitialize)(this.kreditBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOtplatnaTablica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.otplatnatablicaBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -102,5 +192,15 @@
         private System.Windows.Forms.DataGridView dgvOtplatnaTablica;
         private System.Windows.Forms.Button btnPrintanje;
         private System.Windows.Forms.BindingSource kreditBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn brojMjesecaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kamateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn anuitetDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn otplatnaKvotaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ostatakDugaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn otplaceniDioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kreditidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kreditDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource otplatnatablicaBindingSource;
     }
 }
